4f7a7675e1c5433f1915f6b8a8572cd0ca422cca
myk245 <myk245@nyu.edu>
2020-10-01 23:59:31 -0400

    fetch API

--- Fetch-API-Sandbox/app.js
+++ Fetch-API-Sandbox/app.js
@@ -1,8 +1,23 @@
-getText = () => {
-   fetch('test.txt')
-      .then((resp) =>  {
-      console.log(resp)
-   })
+const table = document.getElementById('results')
+const button = document.getElementById('get_users')
+
+function fetchUsers() {
+   table.innerHTML = ''
+
+   fetch('https://jsonplaceholder.typicode.com/users')
+      .then(resp => resp.json())
+      .then(user => user.forEach(getUserInfo))
 }
 
-document.getElementById('button1').addEventListener('click', getText); 
+function getUserInfo(user) {
+   let name = user.name
+   let phone = user.phone
+
+   let row = table.insertRow(0);
+   row.innerText = "Name: " + `${name}` + " " + "Phone: " + `${phone}`
+
+   //   console.log(user.name)
+   //   console.log(user.phone)
+}
+
+button.addEventListener('click', fetchUsers)
