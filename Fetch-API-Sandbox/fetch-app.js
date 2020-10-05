697a55b6936ae719451ff764d9f0f9a53940be44
myk245 <myk245@nyu.edu>
2020-10-05 17:29:21 -0400

    get request with local JSON data

--- Fetch-API-Sandbox/fetch-app.js
+++ Fetch-API-Sandbox/fetch-app.js
@@ -1,8 +1,32 @@
 document.getElementById('button1').addEventListener('click', getText); 
 
+document.getElementById('button2').addEventListener('click', getJSON); 
+
 function getText() {
    fetch('test.txt')
       .then(response => response.text())
-      .then(data => console.log(data))
-      .catch(error => console.log(error))
+      .then(data => {
+         console.log(data);
+         document.getElementById('output').innerHTML = data;
+      })
+      .catch(error => console.log(error));
+}
+
+// Get Local JSON Data
+function getJSON() {
+   fetch('posts.json')
+      .then(response => response.json())
+      .then(data => {
+         console.log(data)
+
+         let output = ''; 
+         data.forEach(post => {
+            output += `
+            <li>${post.title}</li>
+            <p>${post.body}</p>
+            `
+         })
+         document.getElementById('output').innerHTML = output
+      })
+   .catch(error => console.log(error))
 }
