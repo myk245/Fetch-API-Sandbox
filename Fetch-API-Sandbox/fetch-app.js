81e08186a2afab7c072129561b1873776b0a054a
myk245 <myk245@nyu.edu>
2020-10-05 21:39:08 -0400

    get request from an external API

--- Fetch-API-Sandbox/fetch-app.js
+++ Fetch-API-Sandbox/fetch-app.js
@@ -2,6 +2,8 @@ document.getElementById('button1').addEventListener('click', getText);
 
 document.getElementById('button2').addEventListener('click', getJSON); 
 
+document.getElementById('button3').addEventListener('click', getExternal); 
+
 function getText() {
    fetch('test.txt')
       .then(response => response.text())
@@ -24,6 +26,24 @@ function getJSON() {
             output += `
             <li>${post.title}</li>
             <p>${post.body}</p>
+            `
+         })
+         document.getElementById('output').innerHTML = output
+      })
+   .catch(error => console.log(error))
+}
+
+// Get from External API
+function getExternal() {
+   fetch('https://api.github.com/users')
+      .then(response => response.json())
+      .then(data => {
+         console.log(data)
+         
+         let output = ''
+         data.forEach(user => {
+            output += `
+            <li>${user.login}</li>
             `
          })
          document.getElementById('output').innerHTML = output
